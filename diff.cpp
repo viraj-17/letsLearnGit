85b7e3c4a8d4cbd92287fbc8de6b895ca14eb4c4
viraj <unix@DESKTOP-KMDHOFF.localdomain>
2021-12-28 02:25:04 +0530

    added postive diffrence in diff.cpp

--- diff.cpp
+++ diff.cpp
@@ -2,5 +2,8 @@
 int main(){
 	int a,b;
        cin>>a>>b;
-	cout<<"The difference of "<<a<<" and "<<b<<" is "<<a-b<<endl;
+       if(a-b<0)
+	 cout<<"The difference between "<<a<<" and "<<b<<" is "<<-a+b<<endl;
+       else
+	  cout<<"The difference between "<<a<<" and "<<b<<" is "<<a-b<<endl;
 }	
